2919034f2969d8a02a57250e5287ff06c1727104
Jane Sevastyanova <evgenetesla@gmail.com>
2022-06-21 12:00:52 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(10, 14));
+        System.out.println(calc.mult(28, 7));
+        System.out.println(calc.pow(5, 2));
+    }
+}
